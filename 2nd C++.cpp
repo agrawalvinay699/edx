e695d66f83af2de5375a6522a1f10ae62c6a2856
agrawalvinay699 <67180205+agrawalvinay699@users.noreply.github.com>
2021-06-01 03:50:16 +0530

    Create 2nd C++.cpp
    code code go

---
+++ 2nd C++.cpp
@@ -0,0 +1,11 @@
+//Add two numbers
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int a, b;
+  cout << "please give two no for addition." <<endl;
+  cin >> a >> b;
+  cout << "Addition Value =" << a+b;
+  return 0;
+}
